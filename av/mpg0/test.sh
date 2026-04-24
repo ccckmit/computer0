12953d5098256ded755cba8ef719dbec6e6d3873
ccckmit <ccckmit@gmail.com>
2026-04-24 19:49:19 +0800

    mpg0 pass

--- av/mpg0/test.sh
+++ av/mpg0/test.sh
@@ -1,6 +1,5 @@
 set -x
-# gcc -O2 -o mpeg2ppm mpeg2ppm0.c -lm
-gcc mpeg2ppm0.c -o mpeg2ppm \
-    $(pkg-config --cflags --libs libavcodec libavformat libavutil libswscale)
-./mpeg2ppm test.mpg 200 test.ppm
+gcc mpg0.c -o mpg0 -lm
+./mpg0 test.mpg # 會印出 mpg 的基本資訊
+./mpg0 test.mpg 200 test.ppm # 會將 mpg 的第 200 個影格抽出來
 ffmpeg -i test.ppm test.jpg
